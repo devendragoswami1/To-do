337b91c813b73dc1a0c478729a7cba2401770860
devendragoswami1 <goswamigoswamidevendra@gmail.com>
2024-08-24 17:51:41 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -66,13 +66,57 @@ function App() {
   
     }
   }
+  async function getToDo() {
+    const url = 'https://freeapi-app-production-3059.up.railway.app/api/v1/seed/todos';
   
-  makePost();
+    try {
+      const response = await fetch(url);
+  
+      if (!response.ok) {
+        throw new Error(`Request failed with status ${response.status}`);
+      }
+  
+      const result = await response.json();
+      console.log('Success:', result);
+    } catch (error) {
+      console.error('Error:', error);   
+  
+    }
+  }
+
+
+  // async function makePostOFFLINE() {
+  //   const url = 'http://localhost:8080/api/v1/seed/todos';
+  //   const data = {}; // Empty object for the body (optional)
+  
+  //   try {
+  //     const response = await fetch(url, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Accept': 'application/json',
+  //       },
+  //       // body: JSON.stringify(data), // Include data if required
+  //     });
+  
+  //     if (!response.ok) {
+  //       throw new Error(`Request failed with status ${response.status}`);
+  //     }
+  
+  //     const result = await response.json();
+  //     console.log('Success:', result);
+  //   } catch (error) {
+  //     console.error('Error:', error);   
+  
+  //   }
+  // }
+  
   return (
     <>
       <Header />
       <button onClick={makePostRequest}>create To Do</button>
-      <button onClick={makePost}>seed To Do</button>
+      <button onClick={getToDo}>get all To Do</button>
+      {/* <button onClick={makePost}>seed To Do</button> */}
+      {/* <button onClick={makePostOFFLINE}>seed To makePostOFFLINE Do</button> */}
     </>
   );
 }
